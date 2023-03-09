d7b2dbd6eb2ad712bb73a9c2e7c747a16624dea9
mahmud aremu <aremumahmud2003@gmail.com>
2023-03-09 22:56:10 +0100

    uncommented the insert

--- insertProducts.js
+++ insertProducts.js
@@ -1,25 +1,25 @@
  let s = require('./db')
-     // let docs = [{
-     //     name: 'Fries',
-     //     price: 2000,
-     //     currency: 'N',
-     //     picture: 'https://fileuploadservice.aremzy.repl.co/api/getRawFile/?space=63fb931bb96443f9d0187769&file=fries.png'
-     // }, {
-     //     name: 'Salad',
-     //     price: 2000,
-     //     currency: 'N',
-     //     picture: 'https://fileuploadservice.aremzy.repl.co/api/getRawFile/?space=63fb931bb96443f9d0187769&file=salad.png'
-     // }, {
-     //     name: 'Egusi',
-     //     price: 2000,
-     //     currency: 'N',
-     //     picture: 'https://fileuploadservice.aremzy.repl.co/api/getRawFile/?space=63fb931bb96443f9d0187769&file=egusi.png'
-     // }]
-     // s.insertProducts(docs).then(e => {
-     //     console.log(e)
-     // }).catch(e => {
-     //     console.log(e)
-     // })
+ let docs = [{
+     name: 'Fries',
+     price: 2000,
+     currency: 'N',
+     picture: 'https://fileuploadservice.aremzy.repl.co/api/getRawFile/?space=63fb931bb96443f9d0187769&file=fries.png'
+ }, {
+     name: 'Salad',
+     price: 2000,
+     currency: 'N',
+     picture: 'https://fileuploadservice.aremzy.repl.co/api/getRawFile/?space=63fb931bb96443f9d0187769&file=salad.png'
+ }, {
+     name: 'Egusi',
+     price: 2000,
+     currency: 'N',
+     picture: 'https://fileuploadservice.aremzy.repl.co/api/getRawFile/?space=63fb931bb96443f9d0187769&file=egusi.png'
+ }]
+ s.insertProducts(docs).then(e => {
+     console.log(e)
+ }).catch(e => {
+     console.log(e)
+ })
 
  s.getAllProducts().then(res => {
      console.log(res)
